1773fd57cb5fb3c7647a9c28ae7a2515eed4534a
rifat djibran <rifatdjibran099@gmail.com>
2025-06-05 19:31:39 +0700

    Praktikum 1 & Tugas

--- BinaryTreeMain18.java
+++ BinaryTreeMain18.java
@@ -3,29 +3,39 @@ public class BinaryTreeMain18  {
     public static void main(String[] args) {
         BinaryTree18 bst = new BinaryTree18 ();
 
-        bst.add(new Mahasiswa18 ("244160121", "Ali", "A", 3.57));
-        bst.add(new Mahasiswa18 ("244160221", "Badar", "B", 3.85));
-        bst.add(new Mahasiswa18 ("244160185", "Candra", "C", 3.21));
-        bst.add(new Mahasiswa18 ("244160220", "Dewi", "A", 3.54));
+        // bst.add(new Mahasiswa18 ("244160121", "Ali", "A", 3.57));
+        // bst.add(new Mahasiswa18 ("244160221", "Badar", "B", 3.85));
+        // bst.add(new Mahasiswa18 ("244160185", "Candra", "C", 3.21));
+        // bst.add(new Mahasiswa18 ("244160220", "Dewi", "A", 3.54));
 
-        System.out.println("\nDaftar semua mahasiswa (InOrder Traversal):");
-        bst.traverseInOrder(bst.root);
+        // jawaban tugas 1 
+        bst.addRekursif(new Mahasiswa18("244160121", "Ali", "A", 3.57)); 
+        bst.addRekursif(new Mahasiswa18("244160221", "Badar", "B", 3.85));
+        bst.addRekursif(new Mahasiswa18("244160185", "Candra", "C", 3.21));
+        bst.addRekursif(new Mahasiswa18("244160220", "Dewi", "A", 3.54));
+        bst.addRekursif(new Mahasiswa18("244160131", "Devi", "A", 3.72));
+        bst.addRekursif(new Mahasiswa18("244160205", "Ehsan", "D", 3.37));
+        bst.addRekursif(new Mahasiswa18("244160170", "Fizi", "B", 3.46));
 
-        System.out.println("\nPencarian data mahasiswa:");
-        String hasilCari;
-        System.out.print("Cari mahasiswa dengan IPK 3.54: ");
-        hasilCari = bst.find(3.54) ? "Ditemukan" : "Tidak ditemukan";
-        System.out.println(hasilCari);
+        // System.out.println("\nDaftar semua mahasiswa (InOrder Traversal):");
+        // bst.traverseInOrder(bst.root);
 
-        System.out.print("Cari mahasiswa dengan IPK 3.22: ");
-        hasilCari = bst.find(3.22) ? "Ditemukan" : "Tidak ditemukan";
-        System.out.println(hasilCari);
+        // System.out.println("\nPencarian data mahasiswa:");
+        // String hasilCari;
+        // System.out.print("Cari mahasiswa dengan IPK 3.54: ");
+        // hasilCari = bst.find(3.54) ? "Ditemukan" : "Tidak ditemukan";
+        // System.out.println(hasilCari);
 
-        bst.add(new Mahasiswa18 ("244160131", "Devi", "A", 3.72));
-        bst.add(new Mahasiswa18 ("244160205", "Ehsan", "D", 3.37));
-        bst.add(new Mahasiswa18 ("244160170", "Fizi", "B", 3.46));
+        // System.out.print("Cari mahasiswa dengan IPK 3.22: ");
+        // hasilCari = bst.find(3.22) ? "Ditemukan" : "Tidak ditemukan";
+        // System.out.println(hasilCari);
 
-        System.out.println("\nDaftar semua mahasiswa setelah penambahan 3 mahasiswa:");
+        // bst.add(new Mahasiswa18 ("244160131", "Devi", "A", 3.72));
+        // bst.add(new Mahasiswa18 ("244160205", "Ehsan", "D", 3.37));
+        // bst.add(new Mahasiswa18 ("244160170", "Fizi", "B", 3.46));
+
+
+        // System.out.println("\nDaftar semua mahasiswa setelah penambahan 3 mahasiswa:");
         System.out.println("InOrder Traversal:");
         bst.traverseInOrder(bst.root);
 
@@ -35,10 +45,24 @@ public class BinaryTreeMain18  {
         System.out.println("\nPostOrder Traversal:");
         bst.traversePostOrder(bst.root);
 
-        System.out.println("\nPenghapusan data mahasiswa dengan IPK 3.57:");
-        bst.delete(3.57);
+        // soal 2
+        System.out.println("\nMin IPK:");
+        Mahasiswa18 min = bst.cariMinIPK(); 
+        if (min != null) min.tampilInformasi();
 
-        System.out.println("\nDaftar semua mahasiswa setelah penghapusan (InOrder Traversal):");
-        bst.traverseInOrder(bst.root);
+        // soal 2
+        System.out.println("Max IPK:");
+        Mahasiswa18 max = bst.cariMaxIPK(); 
+        if (max != null) max.tampilInformasi();
+
+        // soal 3
+        System.out.println("\nMahasiswa dengan IPK di atas 3.50:");
+        bst.tampilMahasiswaIPKdiAtas(3.50); 
+
+        // System.out.println("\nPenghapusan data mahasiswa dengan IPK 3.57:");
+        // bst.delete(3.57);
+
+        // System.out.println("\nDaftar semua mahasiswa setelah penghapusan (InOrder Traversal):");
+        // bst.traverseInOrder(bst.root);
     }
 }
